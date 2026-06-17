8a747f7e735f4c9d1a6fa0867ff0f1ef764deef1
Nhatpham12 <nhat88672@gmail.com>
2026-06-17 20:47:30 +0700

    fix: add CORS middleware to backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const rateLimit = require("express-rate-limit");
 const seedAdmin = require("./seeders/seedAdmin");
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.set("trust proxy", 1);
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
